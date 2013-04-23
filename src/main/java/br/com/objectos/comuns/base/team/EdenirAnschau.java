3f806314c0121f2058e362225740b0a180aee90b
Marcio Endo <marcio.endo@objectos.com.br>
2013-04-23 20:01:29 -0300

    objectos :: way :: base
    - updates pom.xml
    - oss stuff
    - new parent version
    - LICENSE
    - way-base 1.0-SNAPSHOT
    - atualiza versão de parent
    - novos membros da equipe

--- src/main/java/br/com/objectos/comuns/base/team/EdenirAnschau.java
+++ src/main/java/br/com/objectos/comuns/base/team/EdenirAnschau.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Objectos, Fábrica de Software LTDA.
+ * Copyright 2013 Objectos, Fábrica de Software LTDA.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -33,7 +33,7 @@ import java.lang.annotation.Target;
   ElementType.ANNOTATION_TYPE,
   ElementType.PACKAGE })
 @Retention(RetentionPolicy.SOURCE)
-public @interface Moe {
+public @interface EdenirAnschau {
 
   String[] value() default "";
 
